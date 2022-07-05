d5e2e595b98030d139cf93188c5b5e3470c8f4d8
xiongcj <chengjiex94@gmail.com>
2022-07-05 14:56:24 +0800

    Data_cleaning v1.0.0

--- main.py
+++ main.py
@@ -50,8 +50,8 @@ def go(config: DictConfig):
 
         if "basic_cleaning" in active_steps:
             _ = mlflow.run(
-                # os.path.join(hydra.utils.get_original_cwd(), "src", "basic_cleaning"),
-                f"{config['main']['src_repository']}/basic_cleaning",
+                os.path.join(hydra.utils.get_original_cwd(), "src", "basic_cleaning"),
+                # f"{config['main']['src_repository']}/basic_cleaning",
                 "main",
                 parameters={
                     "input_artifact": "sample.csv:latest",
